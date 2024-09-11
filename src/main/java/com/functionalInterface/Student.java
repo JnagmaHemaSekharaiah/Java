2aef7f5974869dc8168239108230a23f158642ce
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2024-09-11 23:34:47 +0530

    Functional Interfcae

---
+++ src/main/java/com/functionalInterface/Student.java
@@ -0,0 +1,32 @@
+package com.functionalInterface;
+
+
+@FunctionalInterface
+interface School
+{
+    void name();
+}
+
+//public class Parent implements School
+//{
+//    @Override
+//    public void name()
+//    {
+//        System.out.println("School name....");
+//    }
+//}
+public class Student
+{
+    public static void main(String[] args)
+    {
+        School sc =new School()
+        {
+            @Override
+            public void name()
+            {
+                System.out.println("School name is ....");
+            }
+        };
+        sc.name();
+    }
+}
---
+++ src/main/java/com/telusuko/App.java
@@ -0,0 +1,13 @@
+package com.telusuko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
